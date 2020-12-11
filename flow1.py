1a61e33c437c067a2cc6a544143b1180ac7dfae1
07050675 <n101483@gmail.com>
2020-12-11 17:27:41 +0800

    new file:   flow1.py 	flow2.py

---
+++ flow1.py
@@ -0,0 +1,24 @@
+[
+    {
+        "id": "4326a4fb.71a6bc",
+        "type": "http request",
+        "z": "bdf0dd72.26beb",
+        "name": "",
+        "method": "GET",
+        "ret": "txt",
+        "paytoqs": false,
+        "url": "http://api.mediatek.com/mcs/v2/devices/DxUpYwYV/datachannels/LEDControl/datapoints.csv",
+        "tls": "",
+        "persist": false,
+        "proxy": "",
+        "authType": "",
+        "x": 500,
+        "y": 160,
+        "wires": [
+            [
+                "ceb8b5f3.a305d8",
+                "4c98bf49.1ab28"
+            ]
+        ]
+    }
+]
